5f56945e58ae1add51fda1c636876da454cbda05
Jake Javier <jakejavier70@gmail.com>
2022-01-31 13:59:30 -0500

    modularity example edited to be properly separated by function

--- dosing.py
+++ dosing.py
@@ -5,8 +5,38 @@
              type of medical treatment or diagnostics.
 """
 # code should be written more modular as it's currently confusing and not functional
+# original code
+# def dose_amount():
+#     print("Day One Dosing Guidelines")
+#     print("")
+#     print("Choose diagnosis:")
+#     print("1 - Acute otitis media")
+#     print("2 - Acute bacterial sinusitis")
+#     print("3 - Community-acquired pneumonia")
+#     print("4 - Pharyngitis/tonsilitis")
+#     diagnosis = int(input("Enter a number: "))
+#     print("PATIENT WEIGHT")
+#     print("Enter patient weight followed by units of kg or lb.")
+#     print("Examples:  65.3 lb      21.0 kg")
+#     weight_input = input("Enter weight: ")
+#     weight_data = weight_input.split(" ")
+#     weight = float(weight_data[0])
+#     units = weight_data[1]
+#     if units == "lb":
+#         weight = weight / 2.205
+#     dosages_mg_per_kg = [30, 10, 10, 12]
+#     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
+#     dosage_mg_first_day = weight * dosage_mg_per_kg
+#     print("CORRECT DOSAGE")
+#     print("For a patient weighing {:.1f} kg,".format(weight))
+#     print("  the correct dosage is {:.1f} mg the first day"
+#           .format(dosage_mg_first_day))
+#
+#
+# if __name__ == '__main__':
+#     dose_amount()
 
-def dose_amount():
+def input_data():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -19,6 +49,12 @@ def dose_amount():
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
     weight_input = input("Enter weight: ")
+    # dont do the following, its untestable "daisy chaining"
+    # analysis(weight_input, diagnosis)
+    return weight_input, diagnosis
+
+#     could even break this down more
+def analysis(weight_input,diagnosis):
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
@@ -27,6 +63,10 @@ def dose_amount():
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
+    # output(weight,dosage_mg_first_day)
+    return weight, dosage_mg_first_day
+
+def output(weight,dosage_mg_first_day):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
@@ -34,5 +74,7 @@ def dose_amount():
 
 
 if __name__ == '__main__':
-    dose_amount()
-
+    # input_data()
+    weight_input, diagnosis = input_data()
+    weight, dosage_mg_first_day = analysis(weight_input,diagnosis)
+    output(weight,dosage_mg_first_day)
